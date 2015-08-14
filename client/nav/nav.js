31379ca888a4de0f0392730e464922d5803ab239
eliotwinder <musicismymistress@gmail.com>
2015-08-14 14:22:41 -0700

    events

--- client/nav/nav.js
+++ client/nav/nav.js
@@ -2,11 +2,15 @@ angular.module('app.nav',[])
 .controller('loggedInNavController', ['$scope','$location', '$window', 'HttpRequests', 'Auth', function($scope, $location, $window, HttpRequests, Auth){
   $scope.showRequests = false;
   $scope.showEvents = false;
-  
+  $scope.years = [2015,2016,2017];
+  $scope.months = [1,2,3,4,5,6,7,8,9,10,11,12];
+  $scope.days = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
+
   $scope.request = {};
   $scope.ev = {
     organizer: $window.localStorage.getItem('uid')
   };
+  $scope.date = {};
 
   $scope.redirect = function(newpath){
     $location.path(newpath);
@@ -38,10 +42,15 @@ angular.module('app.nav',[])
     $scope.ev.hashtags = $scope.ev.hashtags.split(' ');
   };
 
+  $scope.parseDate = function(){
+    $scope.ev.date = new Date($scope.date.year, $scope.date.month, $scope.date.day);
+  };
+
   $scope.sendPostEvent = function(){
     console.log('before',$scope.ev.hashtags);
     $scope.togglePostEvent();
     $scope.parseHashtags();
+    $scope.parseDate();
     console.log('after',$scope.ev.hashtags);
     HttpRequests.postEvent($scope.ev)
       .then(function(data){
