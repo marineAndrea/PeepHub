9a6262f2dec7edc5c43d36b02fcf9d5611a6dbb8
eliotwinder <musicismymistress@gmail.com>
2015-08-14 12:12:26 -0700

    profilepic

--- DB/user.js
+++ DB/user.js
@@ -29,6 +29,7 @@ var userSchema = mongoose.Schema({
   // name: { type: String, required: true},
   username: { type: String, required: true, index: { unique: true } },
   uid: { type: String, required: true },
+  profilepic: { type: String },
   // password: { type: String },
   location: { type: String },
   email: {type: String, index: { unique: true }},
